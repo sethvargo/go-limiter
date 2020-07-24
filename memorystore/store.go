4d51762dc966526fc1d7c3a466e9438a2792a831
Seth Vargo <seth@sethvargo.com>
2020-07-24 12:00:03 -0400

    Move fasttime internal (#6)
    People should not be directly depending or testing fasttime as it's
    implementation only works for this use case.

--- memorystore/store.go
+++ memorystore/store.go
@@ -8,7 +8,6 @@ import (
 	"unsafe"
 
 	"github.com/sethvargo/go-limiter"
-	"github.com/sethvargo/go-limiter/fasttime"
 )
 
 var _ limiter.Store = (*store)(nil)
@@ -182,7 +181,7 @@ func (s *store) purge() {
 		}
 
 		s.dataLock.Lock()
-		now := fasttime.Now()
+		now := fastnow()
 		for k, b := range s.data {
 			lastTick := (*bucketState)(atomic.LoadPointer(&b.bucketState)).lastTick
 			lastTime := b.startTime + (lastTick * uint64(b.interval))
@@ -232,7 +231,7 @@ type bucketState struct {
 // newBucket creates a new bucket from the given tokens and interval.
 func newBucket(tokens uint64, interval time.Duration, rate float64) *bucket {
 	b := &bucket{
-		startTime: fasttime.Now(),
+		startTime: fastnow(),
 		maxTokens: tokens,
 		interval:  interval,
 		fillRate:  rate,
@@ -251,7 +250,7 @@ func newBucket(tokens uint64, interval time.Duration, rate float64) *bucket {
 func (b *bucket) take() (uint64, uint64, uint64, bool) {
 	// Capture the current request time, current tick, and amount of time until
 	// the bucket resets.
-	now := fasttime.Now()
+	now := fastnow()
 	currTick := tick(b.startTime, now, b.interval)
 	next := b.startTime + ((currTick + 1) * uint64(b.interval))
 
@@ -313,4 +312,15 @@ func availableTokens(last, curr, max uint64, fillRate float64) uint64 {
 // yet.
 func tick(start, curr uint64, interval time.Duration) uint64 {
 	return (curr - start) / uint64(interval)
+}
+
+//go:noescape
+//go:linkname walltime runtime.walltime
+func walltime() (int64, int32)
+
+// fastnow returns a monotonic clock value. The actual value will differ across
+// systems, but that's okay because we generally only care about the deltas.
+func fastnow() uint64 {
+	x, y := walltime()
+	return uint64(x)*1e9 + uint64(y)
 }
