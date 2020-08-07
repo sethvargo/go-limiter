d77f372b586f06b342bae2beaeca696257f49748
Seth Vargo <seth@sethvargo.com>
2020-08-07 09:55:13 -0400

    CI

--- internal/fasttime/fasttime_windows.go
+++ internal/fasttime/fasttime_windows.go
@@ -7,5 +7,5 @@ import "time"
 // Now returns a monotonic clock value. On Windows, no such clock exists, so we
 // fallback to time.Now().
 func Now() uint64 {
-	uint64(time.Now().Nanoseconds())
+	return uint64(time.Now().UnixNano())
 }
