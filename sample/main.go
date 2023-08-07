399cee74b4da90120429355caba3c8387e793708
Sujit D'Mello <sujit@hotmail.com>
2023-08-07 16:33:00 -0400

    Sleep when no tokens avail
    Signed-off-by: Sujit D'Mello <sujit@hotmail.com>

--- sample/main.go
+++ sample/main.go
@@ -15,7 +15,7 @@ func main() {
 		Tokens: 15,
 
 		// Interval until tokens reset.
-		Interval: time.Minute,
+		Interval: time.Second * 5,
 	})
 	if err != nil {
 		log.Fatal(err)
@@ -30,7 +30,13 @@ func main() {
 	for i := 0; i < 20; i++ {
 		// Take a token.
 		tokens, remaining, reset, ok, err := store.Take(ctx, key)
+		if (!ok && remaining == 0) || err != nil {
+			// Rate limit exceeded.
+			tokenAvailableTime := time.Unix(0, int64(reset))
+			fmt.Println("Rate limit exceeded - waiting until", tokenAvailableTime)
+			time.Sleep(time.Until(tokenAvailableTime))
+			tokens, remaining, reset, ok, err = store.Take(ctx, key)
+		}
 		fmt.Println(tokens, remaining, reset, ok, err)
 	}
-
 }
