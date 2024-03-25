3931e26d715736c94d99b2e302116d81d75eaa5e
Seth Vargo <seth@sethvargo.com>
2024-03-25 16:58:17 -0400

    Fix comment
    Fixes #39

--- internal/fasttime/fasttime.go
+++ internal/fasttime/fasttime.go
@@ -11,8 +11,9 @@ import (
 //go:linkname now time.now
 func now() (sec int64, nsec int32, mono int64)
 
-// Now returns a monotonic clock value. The actual value will differ across
-// systems, but that's okay because we generally only care about the deltas.
+// Now returns a monotonic and wall clock value. The actual value will differ
+// across systems, but that's okay because we generally only care about the
+// deltas.
 func Now() uint64 {
 	sec, nsec, _ := now()
 	return uint64(sec)*1e9 + uint64(nsec)
