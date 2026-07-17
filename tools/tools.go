461e7692368bad27821f4f2162079c50dd068591
Seth Vargo <seth@sethvargo.com>
2026-07-17 15:33:29 -0400

    Drop dev-tooling module and lint checks
    Remove the tools/ module (goimports, staticcheck, misspell), the fmtcheck,
    staticcheck, and spellcheck Makefile targets, and the CI Lint step. Keep the
    workflow to just go test, matching go-envconfig. gofmt and go vet still run
    via the test targets.

--- tools/tools.go
+++
@@ -1,8 +0,0 @@
-// Package tools includes the list of tools used in the project.
-package tools
-
-import (
-	_ "github.com/client9/misspell/cmd/misspell"
-	_ "golang.org/x/tools/cmd/goimports"
-	_ "honnef.co/go/tools/cmd/staticcheck"
-)
